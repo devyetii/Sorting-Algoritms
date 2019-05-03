02026fc6fae940c4838df118abde4016fb897037
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2019-05-04 01:24:47 +0200

    Add Selection Sort

--- Sorting/Sorting/SortingFunctions.cpp
+++ Sorting/Sorting/SortingFunctions.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<iterator>
+#include<algorithm> //From which we use the method std::swap()
 
 //Helping function to get the array size
 //source: https://www.quora.com/What-is-the-equivalent-of-array-length-used-in-Java-in-C++
@@ -16,15 +17,40 @@ void Print(const T (&arr)[N])
 	cout << '\n';
 }
 
-template<typename T, std::size_t N>
-void InsertionSort(T (&arr)[N])
-{
 
+/*
+** (Internal) : Selection Sort 
+** params : arr->Array to be sorted, ord-> 0 for Asc., 1 for Desc.
+** Complexity : O(n^2), n->amount of sorted data
+** Order : Ascending
+** Stability: Stable
+** Find algorithm and explanation in the Reference Data Structures: A Pseudocode Approach Using C, 2nd ed.
+** Section 12.2 - p: 537
+*/
+template<typename T, std::size_t N>
+void SelectionSort(T (&arr)[N], bool ord)
+{
+	int swap_idx = 0; //represents the index of the smallest element in the unsorted list
+	for (int i = 0; i < N-1; ++i) {
+		swap_idx = i;
+		for (int j = i+1; j < N; ++j) {
+			if ((ord ? arr[j] > arr[swap_idx] : arr[j] <arr[swap_idx]))
+				swap_idx = j;
+		}
+		std::swap(arr[i], arr[swap_idx]); 
+	}
 }
 
 
+/*
+** (Internal) : Insertion Sort 
+** Order : Ascending
+** Stability: Stable
+** Find algorithm and explanation in the Reference Data Structures: A Pseudocode Approach Using C, 2nd ed.
+** Page 
+*/ 
 template<typename T, std::size_t N>
-void SelectionSort(T (&arr)[N])
+void InsertionSort(T (&arr)[N])
 {
 	
 }
--- Sorting/Sorting/main.cpp
+++ Sorting/Sorting/main.cpp
@@ -3,10 +3,34 @@
 #include<string>
 using namespace std;
 
+//A user-defined struct to work with
+template<typename Tf, typename Ts>
+struct Pair {
+	Tf f;
+	Ts s;
+	Pair(Tf f_param, Ts s_param) : f(f_param) : s(s_param) {};
+	bool operator <(const Pair<Ts, Tf>& other) {
+		return this->f  < other->f;
+	};
+	bool operator >(const Pair<Ts, Tf>& other) {
+		return this->f  > other->f;
+	}
+};
+
+
 int main()
 {
-	int a[] = {1, 56, 98, -124, 0};
-	InsertionSort(a);
+	int a[] = {1, 56, 98, 98, -124, 0};
+	cout << "Initially : ";
 	Print(a);
+
+	cout << "Applying Selection Sort, Ascending\n";
+	SelectionSort(a, 0);
+	Print(a);
+
+	cout << "Applying Selection Sort, Descending\n";
+	SelectionSort(a, 1);
+	Print(a);
+	
 	system("pause");
 }
